c301bedd8d2a416d5612dd4742662d9435d050b0
longjie1107 <330557140@qq.com>
2022-01-24 18:32:33 +0800

    editUserInfo done

--- src/components/editUserInfo.tsx
+++ src/components/editUserInfo.tsx
@@ -1,19 +1,48 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button ,Select,Card,Avatar} from "antd";
 
-const Demo = () => {
-    const onFinish = (values: any) => {
-        console.log("Success:", values);
-    };
-
-    const onFinishFailed = (errorInfo: any) => {
-        console.log("Failed:", errorInfo);
-    };
-};
+const { Option } = Select;
+const { Meta } = Card;
 
 export default function EditUserInfo(): JSX.Element {
+    // const [form] = Form.useForm();
+
+    // const onGenderChange = (value: string) => {
+    //     switch (value) {
+    //       case 'male':
+    //         form.setFieldsValue({ note: 'Hi, man!' });
+    //         return;
+    //       case 'female':
+    //         form.setFieldsValue({ note: 'Hi, lady!' });
+    //         return;
+    //       case 'other':
+    //         form.setFieldsValue({ note: 'Hi there!' });
+    //     }
+    //   };
+    
+    // const onFinish = (values: any) => {
+    // console.log(values);
+    // };
+
+    // const onReset = () => {
+    // form.resetFields();
+    // };
+
+    // const onFill = () => {
+    // form.setFieldsValue({
+    //     note: 'Hello world!',
+    //     gender: 'male',
+    // });
+    // };
+
     return (
         <>
+            <Meta
+                avatar={<Avatar size={48} src="https://joeschmoe.io/api/v1/random" />}
+                title="User Nickname"
+                description="This is the description"
+                style={{marginLeft: "20%"}}
+            />
             <Form
                 name="basic"
                 labelCol={{ span: 8 }}
@@ -23,6 +52,19 @@ export default function EditUserInfo(): JSX.Element {
                 // onFinishFailed={onFinishFailed}
                 autoComplete="off"
             >
+                <Form.Item
+                    label="Nickname"
+                    name="Nickname"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your nickname!",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
                 <Form.Item
                     label="Username"
                     name="username"
@@ -37,25 +79,55 @@ export default function EditUserInfo(): JSX.Element {
                 </Form.Item>
 
                 <Form.Item
-                    label="Password"
-                    name="password"
+                    label="Brief"
+                    name="brief"
                     rules={[
                         {
                             required: true,
-                            message: "Please input your password!",
+                            message: "Please input your brief!",
                         },
                     ]}
                 >
-                    <Input.Password />
+                    <Input />
                 </Form.Item>
 
-                {/* <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{ offset: 8, span: 16 }}
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your email!",
+                        },
+                    ]}
                 >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item> */}
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Phone Number"
+                    name="phoneNumber"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your phone number!",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+                    <Select
+                    placeholder="Select a option and change input text above"
+                    // onChange={onGenderChange}
+                    allowClear
+                    >
+                    <Option value="male">male</Option>
+                    <Option value="female">female</Option>
+                    <Option value="other">other</Option>
+                    </Select>
+                </Form.Item>
 
                 <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                     <Button type="primary" htmlType="submit">
