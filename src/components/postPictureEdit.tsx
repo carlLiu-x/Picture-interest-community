a58567169f916be303a0d72c24f9811cc8d1794e
lishikaihaha <51074865+lishikaihaha@users.noreply.github.com>
2022-02-24 16:10:55 +0800

    post_picture_notes

--- src/components/postPictureEdit.tsx
+++ src/components/postPictureEdit.tsx
@@ -11,7 +11,7 @@ export class PostPictureEdit extends Component {
   render() {
     return(
         <Layout>
-            {/**图片编辑导航栏路由链接*/}
+            {/**图片编辑左侧导航栏路由链接*/}
             <Sider className="picture-edit-item" width={100} theme = "light">
               <Content className="picture-edit-item-content">
                 <NavLink className="cut" to="/mainPage/send/edit/postpicturecut" >
@@ -35,12 +35,15 @@ export class PostPictureEdit extends Component {
                 </NavLink>
               </Content>
             </Sider>
-            {/**图片编辑注册路由*/}
+            {/**图片编辑内容路由*/}
             <Content className="picture-edit">
             <Routes>
               <Route path="/" element={<PostPictureCut />}/>
+              {/* 裁剪 */}
               <Route path="/postpicturecut" element={<PostPictureCut />}/>
+              {/* 滤镜 */}
               <Route path="/postpicturefilter" element={<PostPictureFilter />}/>
+              {/* 标记 */}
               <Route path="/postpicturesign" element={<PostPictureSign />}/>
             </Routes>
             </Content>            
